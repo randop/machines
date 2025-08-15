0d9f033827de98f425164ba267938e9b93e87baf
Randolph Ledesma <randopledesma+gitlab@hotmail.com>
2025-08-15 15:41:08 +0000

    Add dnsdist setup and configuration

--- routers-deuteronomy-v2/setup.sh
+++ routers-deuteronomy-v2/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-pacman -S powerdns dnsmasq
+pacman -S powerdns dnsmasq dnsdist openbsd-netcat
 
 # powerdns setup
 mariadb -u pdns -p pdns </usr/share/doc/powerdns/schema.mysql.sql
