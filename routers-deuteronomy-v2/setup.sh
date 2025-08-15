96a5fc5f4aad70046cbe6a0b0b7d8bce231dfca2
Randolph Ledesma <randopledesma+gitlab@hotmail.com>
2025-08-15 15:52:49 +0000

    Configure systemd boot dnsmasq powerdns dnsdist

--- routers-deuteronomy-v2/setup.sh
+++ routers-deuteronomy-v2/setup.sh
@@ -5,3 +5,6 @@ pacman -S powerdns dnsmasq dnsdist openbsd-netcat
 
 # powerdns setup
 mariadb -u pdns -p pdns </usr/share/doc/powerdns/schema.mysql.sql
+
+# enable services
+systemctl enable --now dnsmasq pdns dnsdist
