4bfd7fee38a2cc5ec2ddfea4333bfa15693a42ac
Randolph Ledesma <randopledesma+gitlab@hotmail.com>
2025-08-15 16:58:48 +0000

    Setup mariadb

--- routers-deuteronomy-v2/setup.sh
+++ routers-deuteronomy-v2/setup.sh
@@ -3,6 +3,15 @@ set -euo pipefail
 
 pacman -S powerdns dnsmasq dnsdist openbsd-netcat
 
+# mariadb setup
+mariadb-install-db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
+mkdir -p /var/log/mariadb
+chown mysql:mysql /var/log/mariadb
+chmod 750 /var/log/mariadb
+systemctl enable --now mariadb
+mariadb-tzinfo-to-sql /usr/share/zoneinfo | mariadb -u root -p mysql
+mariadb-check mysql
+
 # powerdns setup
 mariadb -u pdns -p pdns </usr/share/doc/powerdns/schema.mysql.sql
 
