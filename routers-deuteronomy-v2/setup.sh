365f3ebf97eab130a3444f870cd957d7a4a6b926
Randolph Ledesma <randopledesma+gitlab@hotmail.com>
2025-08-16 08:31:16 +0000

    Upgrade to MariaDB version 12.0.2

--- routers-deuteronomy-v2/setup.sh
+++ routers-deuteronomy-v2/setup.sh
@@ -3,6 +3,10 @@ set -euo pipefail
 
 pacman -S powerdns dnsmasq dnsdist openbsd-netcat mariadb
 
+# dnsmasq setup
+mkdir -p /etc/dnsmasq.d/enabled
+mkdir -p /etc/dnsmasq.d/available
+
 # mariadb setup
 mariadb-install-db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
 mkdir -p /var/log/mariadb
@@ -11,6 +15,9 @@ chmod 750 /var/log/mariadb
 systemctl enable --now mariadb
 mariadb-tzinfo-to-sql /usr/share/zoneinfo | mariadb -u root -p mysql
 mariadb-check mysql
+
+# :: MariaDB was updated to a new feature release. To update the data run:
+# systemctl restart mariadb.service && mariadb-upgrade -u root -p
 
 # powerdns setup
 mariadb -u pdns -p pdns </usr/share/doc/powerdns/schema.mysql.sql
