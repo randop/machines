c41482804a465c1d3243315938b87dfcb0a44aec
Randolph Ledesma <randopledesma+gitlab@hotmail.com>
2025-08-16 10:18:43 +0000

    Import intranet domains using sql artifact

---
+++ routers-deuteronomy-v2/domains.sql
@@ -0,0 +1,8 @@
+USE pdns;
+INSERT INTO domains (name, type) VALUES ('tindango', 'NATIVE');
+INSERT INTO domains (name, type) VALUES ('intranet', 'NATIVE');
+INSERT INTO domains (name, type) VALUES ('localdomain', 'NATIVE');
+
+
+
+
