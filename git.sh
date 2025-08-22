99fd0b5ab8d5460cc821eb2a4f53f4e0c8d6bd88
Randolph Ledesma <randopledesma+gitlab@hotmail.com>
2025-08-22 03:55:20 +0000

    Update git remote multi-origins

--- git.sh
+++ git.sh
@@ -7,6 +7,6 @@ git config --local user.name "Randolph Ledesma"
 git config --local user.email "randolph@email.com"
 git remote add origin git@gitlab.com:randop/machines.git
 git push --set-upstream origin master
-git remote set-url --add --push origin git@github.com:randop/machines.git
+git remote set-url --add --push origin git@gitlab.com:randop/machines.git
 git remote set-url --add --push origin git@github.com:randop/machines.git
 git push --set-upstream origin master
