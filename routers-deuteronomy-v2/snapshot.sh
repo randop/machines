cb42e7e80961c0576530f4d6870110fe16718c83
Randolph <randopledesma+gitlab@hotmail.com>
2026-01-16 18:07:11 +0000

    Upgrade router v2 with new system packages

--- routers-deuteronomy-v2/snapshot.sh
+++ routers-deuteronomy-v2/snapshot.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-#  Copyright © 2010 — 2025 Randolph Ledesma
+#  Copyright © 2010 — 2026 Randolph Ledesma
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -19,17 +19,17 @@ set -euo pipefail
 
 # Get Arch Linux information
 uname -a
-# Linux markpeter 6.18.3-arch1-1 #1 SMP PREEMPT_DYNAMIC Fri, 02 Jan 2026 17:52:55 +0000 x86_64 GNU/Linux
+# Linux markpeter 6.18.5-arch1-1 #1 SMP PREEMPT_DYNAMIC Sun, 11 Jan 2026 17:10:53 +0000 x86_64 GNU/Linux
 
 hostnamectl
 #  Static hostname: markpeter
 #        Icon name: computer-desktop
 #          Chassis: desktop 🖥
 #       Machine ID: 00000000000000000000000000000000
-#          Boot ID: f5444ed6af9b4313b13a283518aa8e31
+#          Boot ID: 96072b8694bc4ecf94247e5297ecbc6d
 #     Product UUID: 00000000-0000-0000-0000-000000080000
 # Operating System: Arch Linux
-#           Kernel: Linux 6.18.3-arch1-1
+#           Kernel: Linux 6.18.5-arch1-1
 #     Architecture: x86-64
 #  Hardware Vendor: AMI Corporation
 #   Hardware Model: Aptio CRB
@@ -46,7 +46,7 @@ mariadb -e "SELECT VERSION();"
 # +----------------+
 
 dnsmasq --version
-# Dnsmasq version 2.91  Copyright (c) 2000-2025 Simon Kelley
+# Dnsmasq version 2.92  Copyright (c) 2000-2025 Simon Kelley
 # Compile time options: IPv6 GNU-getopt DBus no-UBus i18n IDN2 DHCP DHCPv6 no-Lua TFTP conntrack ipset nftset auth DNSSEC loop-detect inotify dumpfile
 
 pdns_server --version
@@ -72,21 +72,20 @@ kea-dhcp4 -V
 # - Memfile backend 3.0
 
 dnsdist -V
-# dnsdist 2.0.2 (Lua 5.1.4 [LuaJIT 2.1.1764270282])
+# dnsdist 2.0.2 (Lua 5.1.4 [LuaJIT 2.1.1765228720])
 # Enabled features: AF_XDP cdb dns-over-quic dns-over-http3 dns-over-tls(gnutls openssl) dns-over-https(nghttp2) dnscrypt ebpf fstrm ipcipher libedit libsodium lmdb protobuf re2 recvmmsg/sendmmsg snmp systemd yaml
 
 iperf3 --version
 # iperf 3.20 (cJSON 1.7.15)
-# Linux markpeter 6.18.1-arch1-2 #1 SMP PREEMPT_DYNAMIC Sat, 13 Dec 2025 18:23:21 +0000 x86_64
 # Optional features available: CPU affinity setting, IPv6 flow label, SCTP, TCP congestion algorithm setting, sendfile / zerocopy, socket pacing, authentication, bind to device, support IPv4 don't fragment, POSIX threads
 
 df -h
 # Filesystem      Size  Used Avail Use% Mounted on
-# /dev/sda2       117G  6.1G  105G   6% /
+# /dev/sda2       117G  7.1G  104G   7% /
 # devtmpfs        1.9G     0  1.9G   0% /dev
 # tmpfs           1.9G     0  1.9G   0% /dev/shm
-# efivarfs        128K  105K   19K  86% /sys/firmware/efi/efivars
-# tmpfs           748M  876K  747M   1% /run
+# efivarfs        128K  119K  5.0K  96% /sys/firmware/efi/efivars
+# tmpfs           748M  932K  747M   1% /run
 # tmpfs           1.9G     0  1.9G   0% /tmp
 # none            1.0M     0  1.0M   0% /run/credentials/systemd-journald.service
 # /dev/sda1       511M   34M  478M   7% /boot
@@ -143,4 +142,4 @@ NetworkManager -V
 # 1.54.3-1
 
 bird --version
-# BIRD version 3.1.5
+# BIRD version 3.2.0
