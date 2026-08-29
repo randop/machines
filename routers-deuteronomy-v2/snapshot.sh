88e79f2167908a8c14ec44dc1296a75a052e2a41
Randolph Ledesma <randopledesma+gitlab@hotmail.com>
2026-08-29 06:14:40 +0000

    router(v2): system post-upgrade snapshot information

--- routers-deuteronomy-v2/snapshot.sh
+++ routers-deuteronomy-v2/snapshot.sh
@@ -19,17 +19,17 @@ set -euo pipefail
 
 # Get Arch Linux information
 uname -a
-# Linux markpeter 6.19.8-arch1-1 #1 SMP PREEMPT_DYNAMIC Sat, 14 Mar 2026 01:07:43 +0000 x86_64 GNU/Linux
+# Linux markpeter 6.18.48-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 28 Aug 2026 11:47:30 +0000 x86_64 GNU/Linux
 
 hostnamectl
 #  Static hostname: markpeter
 #        Icon name: computer-desktop
 #          Chassis: desktop 🖥
 #       Machine ID: 00000000000000000000000000000000
-#          Boot ID: f0656ebbfd114442aacc5b6427e59a7c
+#          Boot ID: 62075f11697c4c30856c630581066b8c
 #     Product UUID: 00000000-0000-0000-0000-000000080000
 # Operating System: Arch Linux
-#           Kernel: Linux 6.19.8-arch1-1
+#           Kernel: Linux 6.18.48-1-lts
 #     Architecture: x86-64
 #  Hardware Vendor: AMI Corporation
 #   Hardware Model: Aptio CRB
@@ -42,56 +42,55 @@ mariadb -e "SELECT VERSION();"
 # +----------------+
 # | VERSION()      |
 # +----------------+
-# | 12.2.2-MariaDB |
+# | 12.3.3-MariaDB |
 # +----------------+
 
 dnsmasq --version
-# Dnsmasq version 2.92  Copyright (c) 2000-2025 Simon Kelley
+# Dnsmasq version 2.93  Copyright (c) 2000-2026 Simon Kelley
 # Compile time options: IPv6 GNU-getopt DBus no-UBus i18n IDN2 DHCP DHCPv6 no-Lua TFTP conntrack ipset nftset auth DNSSEC loop-detect inotify dumpfile
 
 pdns_server --version
-# Nov 26 06:45:45 Loading '/usr/lib/powerdns/libremotebackend.so'
-# Nov 26 06:45:45 Loading '/usr/lib/powerdns/libpipebackend.so'
-# Nov 26 06:45:45 Loading '/usr/lib/powerdns/libldapbackend.so'
-# Nov 26 06:45:45 Loading '/usr/lib/powerdns/libgpgsqlbackend.so'
-# Nov 26 06:45:45 Loading '/usr/lib/powerdns/liblmdbbackend.so'
-# Nov 26 06:45:45 Loading '/usr/lib/powerdns/libgeoipbackend.so'
-# Nov 26 06:45:45 Unable to load module '/usr/lib/powerdns/libgeoipbackend.so': libyaml-cpp.so.0.8: cannot open shared object file: No such file or directory
-# Nov 26 06:45:45 DNSBackend unable to load module in libgeoipbackend.so
-# PowerDNS Authoritative Server 5.0.3 (C) PowerDNS.COM BV
-# Using 64-bits mode. Built using gcc 15.2.1 20260209.
+# Aug 29 06:01:02 Loading '/usr/lib/powerdns/libremotebackend.so'
+# Aug 29 06:01:02 Loading '/usr/lib/powerdns/libpipebackend.so'
+# Aug 29 06:01:02 Loading '/usr/lib/powerdns/libldapbackend.so'
+# Aug 29 06:01:02 Loading '/usr/lib/powerdns/libgpgsqlbackend.so'
+# Aug 29 06:01:02 Loading '/usr/lib/powerdns/liblmdbbackend.so'
+# Aug 29 06:01:02 Loading '/usr/lib/powerdns/libgeoipbackend.so'
+# Aug 29 06:01:02 Unable to load module '/usr/lib/powerdns/libgeoipbackend.so': libyaml-cpp.so.0.9: cannot open shared object file: No such file or directory
+# Aug 29 06:01:02 DNSBackend unable to load module in libgeoipbackend.so
+# PowerDNS Authoritative Server 5.1.4 (C) PowerDNS.COM BV
+# Using 64-bits mode. Built using gcc 16.2.1 20260810.
 # PowerDNS comes with ABSOLUTELY NO WARRANTY. This is free software, and you are welcome to redistribute it according to the terms of the GPL version 2.
 
 kea-dhcp4 -V
-# 3.0.2-git (3.0.2-git (git 0b0b77df4940cbe400ba3801db5aa1b5dcd58c79))
+# 3.2.0-git (git e1855213f333e96f65b952676cc03f93942b1e57)
 # premium: no
 # linked with:
-# - log4cplus 2.1.0
-# - OpenSSL 3.6.0 1 Oct 2025
+# - log4cplus 2.2.0
+# - OpenSSL 3.6.4 25 Aug 2026
 # lease backends:
 # - Memfile backend 3.0
 
 dnsdist -V
-# dnsdist 2.0.3 (Lua 5.1.4 [LuaJIT 2.1.1774896198])
-# Enabled features: AF_XDP cdb dns-over-quic dns-over-http3 dns-over-tls(gnutls openssl) dns-over-https(nghttp2) dnscrypt ebpf fstrm ipcipher libedit libsodium lmdb protobuf re2 recvmmsg/sendmmsg snmp systemd yaml
+# dnsdist 2.1.1 (Lua 5.1.4 [LuaJIT 2.1.1785763465])
+# Enabled features: AF_XDP cdb dns-over-quic dns-over-http3 dns-over-tls(gnutls openssl) dns-over-https(nghttp2) dnscrypt ebpf fstrm ipcipher ipcrypt2 libedit libsodium lmdb protobuf re2 recvmmsg/sendmmsg snmp systemd yaml
 
 iperf3 --version
-# iperf 3.20 (cJSON 1.7.15)
-# Optional features available: CPU affinity setting, IPv6 flow label, SCTP, TCP congestion algorithm setting, sendfile / zerocopy, socket pacing, authentication, bind to device, support IPv4 don't fragment, POSIX threads
+# iperf 3.21 (cJSON 1.7.15)
+# Optional features available: CPU affinity setting, IPv6 flow label, SCTP, TCP congestion algorithm setting, sendfile / zerocopy, socket pacing, authentication, bind to device, support IPv4 don't fragment, POSIX threads, GSO/GRO support
 
 df -h
 # Filesystem      Size  Used Avail Use% Mounted on
-# /dev/sda2       117G  8.0G  103G   8% /
-# devtmpfs        1.8G     0  1.8G   0% /dev
+# /dev/sda2       117G  9.8G  101G   9% /
+# devtmpfs        1.9G     0  1.9G   0% /dev
 # tmpfs           1.9G     0  1.9G   0% /dev/shm
-# efivarfs        128K   33K   91K  27% /sys/firmware/efi/efivars
-# tmpfs           748M  900K  747M   1% /run
+# efivarfs        128K  127K     0 100% /sys/firmware/efi/efivars
+# tmpfs           760M  896K  759M   1% /run
 # tmpfs           1.9G     0  1.9G   0% /tmp
 # none            1.0M     0  1.0M   0% /run/credentials/systemd-journald.service
-# /dev/sda1       511M   64M  448M  13% /boot
+# /dev/sda1       511M   69M  443M  14% /boot
 # none            1.0M     0  1.0M   0% /run/credentials/getty@tty1.service
-# tmpfs           374M  4.0K  374M   1% /run/user/0
-# tmpfs           374M  4.0K  374M   1% /run/user/1000
+# tmpfs           380M  4.0K  380M   1% /run/user/1000
 
 fdisk -l
 # Disk /dev/sda: 119.24 GiB, 128035676160 bytes, 250069680 sectors
@@ -108,11 +107,12 @@ fdisk -l
 
 pacman -Qi refind
 # Name            : refind
-# Version         : 0.14.2-1
+# Version         : 0.14.2-3
 # Description     : An EFI boot manager
 # Architecture    : x86_64
 # URL             : https://www.rodsbooks.com/refind/
-# Licenses        : BSD-2-Clause  CC-BY-SA-3.0  CC-BY-SA-4.0  GPL-2.0-only  GPL-2.0-or-later  GPL-3.0-or-later  LGPL-2.1-or-later  LGPL-3.0-or-later OR CC-BY-SA-3.0
+# Licenses        : BSD-2-Clause  CC-BY-SA-3.0  CC-BY-SA-4.0  GPL-2.0-only  GPL-2.0-or-later  GPL-3.0-or-later  LGPL-2.1-or-later
+#                   LGPL-3.0-or-later OR CC-BY-SA-3.0
 # Groups          : None
 # Provides        : None
 # Depends On      : bash  dosfstools  efibootmgr
@@ -127,10 +127,10 @@ pacman -Qi refind
 # Optional For    : None
 # Conflicts With  : None
 # Replaces        : None
-# Installed Size  : 1890.15 KiB
-# Packager        : David Runge <dvzrv@archlinux.org>
-# Build Date      : Sun 07 Apr 2024 08:13:16 AM UTC
-# Install Date    : Thu 14 Aug 2025 01:29:19 PM UTC
+# Installed Size  : 1930.69 KiB
+# Packager        : Campbell Jones <serebit@archlinux.org>
+# Build Date      : Wed 29 Apr 2026 04:14:23 AM UTC
+# Install Date    : Sat 29 Aug 2026 05:47:44 AM UTC
 # Install Reason  : Explicitly installed
 # Install Script  : No
 # Validated By    : Signature
@@ -140,19 +140,19 @@ ufw --version
 # Copyright 2008-2023 Canonical Ltd.
 
 NetworkManager -V
-# 1.56.0-1
+# 1.58.1-1
 
 bird --version
-# BIRD version 3.2.0
+# BIRD version 3.2.1
 
 hostapd -v
-# hostapd v2.11-hostap_2_11+
+# hostapd v2.12-hostap_2_12+
 # User space daemon for IEEE 802.11 AP management,
 # IEEE 802.1X/WPA/WPA2/EAP/RADIUS Authenticator
-# Copyright (c) 2002-2024, Jouni Malinen <j@w1.fi> and contributors
+# Copyright (c) 2002-2026, Jouni Malinen <j@w1.fi> and contributors
 
 sqlite3 --version
-# 3.52.0 2026-03-06 16:01:44 557aeb43869d3585137b17690cb3b64f7de6921774daae9e56403c3717dcalt1 (64-bit)
+# 3.53.4 2026-07-24 19:02:57 bf7c7f30031888f4e796e429ab3978879485813aaca6f641c7b33e4e0945alt1 (64-bit)
 
 pacman -Qi reflector
 # Name            : reflector
@@ -179,7 +179,7 @@ pacman -Qi reflector
 
 pacman -Qi intel-ucode
 # Name            : intel-ucode
-# Version         : 20260227-1
+# Version         : 20260812-1
 # Description     : Microcode update files for Intel CPUs
 # Architecture    : any
 # URL             : https://github.com/intel/Intel-Linux-Processor-Microcode-Data-Files
@@ -192,17 +192,17 @@ pacman -Qi intel-ucode
 # Optional For    : None
 # Conflicts With  : None
 # Replaces        : microcode_ctl
-# Installed Size  : 30.24 MiB
+# Installed Size  : 31.45 MiB
 # Packager        : T.J. Townsend <blakkheim@archlinux.org>
-# Build Date      : Fri 27 Feb 2026 05:22:23 PM UTC
-# Install Date    : Mon 16 Mar 2026 04:57:29 PM UTC
+# Build Date      : Wed 12 Aug 2026 10:04:42 PM UTC
+# Install Date    : Sat 29 Aug 2026 05:47:35 AM UTC
 # Install Reason  : Explicitly installed
 # Install Script  : No
 # Validated By    : Signature
 
 journalctl -k --grep=microcode
-# Mar 16 17:01:12 archlinux kernel: microcode: Current revision: 0x0000090d
-# Mar 16 17:01:12 archlinux kernel: microcode: Updated early from: 0x0000090a
+# Aug 29 05:49:42 archlinux kernel: microcode: Current revision: 0x0000090d
+# Aug 29 05:49:42 archlinux kernel: microcode: Updated early from: 0x0000090a
 
 vnstat --version
-# vnStat 2.13 by Teemu Toivola <tst at iki dot fi> (SQLite 3.52.0)
+# vnStat 2.13 by Teemu Toivola <tst at iki dot fi> (SQLite 3.53.4)
